2e3324363020d00cb1c3124a6e4660dbd9cc9d22
chai <yeshengdemogujun@gmail.com>
2019-10-05 03:45:25 +1000

    ver1.3 generics step1

--- src/db_files/CustomersFile.java
+++ src/db_files/CustomersFile.java
@@ -1,6 +1,6 @@
 package db_files;
 
-public class CustomersFile extends dbFile {
+public class CustomersFile<T> extends dbFile<T> {
 	public CustomersFile() {
 		super();
 	}
--- src/db_files/testing.java
+++ src/db_files/testing.java
@@ -3,5 +3,6 @@ package db_files;
 public class testing {
 	public static void main(String[] args) {
 		CustomersFile file = new CustomersFile();
+		file.print();
 	}
 }
